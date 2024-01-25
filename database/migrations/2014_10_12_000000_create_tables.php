544ec547891d0099c62ed88dbeefd13cb40a95eb
Jeffer48 <herrerajeffer@gmail.com>
2024-01-25 16:03:56 -0600

    Creacion de migraciones

---
+++ database/migrations/2014_10_12_000000_create_tables.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('grupos');
+        Schema::dropIfExists('catalogo');
+        Schema::dropIfExists('reporte_radio');
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('user');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->string('created_user',45)->nullable(false);
+            $table->string('updated_user',45)->nullable(true);
+            $table->string('deleted_user',45)->nullable(true);
+        });
+
+        Schema::create('grupos', function(Blueprint $table) {
+            $table->id();
+            $table->string('grupo')->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->string('created_user',45)->nullable(false);
+            $table->string('updated_user',45)->nullable(true);
+            $table->string('deleted_user',45)->nullable(true);
+        });
+
+        Schema::create('catalogo', function(Blueprint $table) {
+            $table->id();
+            $table->integer('id_grupo')->nullable(false);
+            $table->string('descripcion',255)->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->string('created_user',45)->nullable(false);
+            $table->string('updated_user',45)->nullable(true);
+            $table->string('deleted_user',45)->nullable(true);
+            
+            $table->foreign('id_grupo')->references('id')->on('grupos');
+        });
+
+        Schema::create('reporte_radio', function(Blueprint $table) {
+            $table->id();
+            $table->date('fecha')->nullable(false);
+            $table->time('hora')->nullable(false);
+            $table->integer('id_area')->nullable(false);
+            $table->integer('id_unidad')->nullable(false);
+            $table->integer('id_tipo_incidente')->nullable(false);
+            $table->string('ubicacion',255)->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->string('created_user',45)->nullable(false);
+            $table->string('updated_user',45)->nullable(true);
+            $table->string('deleted_user',45)->nullable(true);
+
+            $table->foreign('id_area')->references('id')->on('catalogo');
+            $table->foreign('id_unidad')->references('id')->on('catalogo');
+            $table->foreign('id_tipo_incidente')->references('id')->on('catalogo');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('grupos');
+        Schema::dropIfExists('catalogo');
+        Schema::dropIfExists('reporte_radio');
+    }
+};
--- resources/views/reporte_radio.blade.php
+++ resources/views/reporte_radio.blade.php
@@ -1,12 +1,92 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-<div>
-    <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">Email address</label>
-        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-      </div>
+<div class="container" style="height: 10rem;">
+    <div class="card bg-light">
+        <div class="card-body">
+        <form class="col-sm-10" >
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Fecha</label>
+                <div class="col-sm-10" style="height: 3rem">
+                    <input type="text" class="form-control" id="inputPassword" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="date" class="col-sm-2 col-form-label">Date</label>
+                <div class="col-sm-10" style="height: 3rem">
+                  <div class="input-group date" id="datepicker">
+                    <input type="text" class="form-control" id="date"/>
+                    <span class="input-group-append">
+                      <span class="input-group-text bg-light d-block">
+                        <i class="fa fa-calendar"></i>
+                      </span>
+                    </span>
+                  </div>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Área</label>
+                <div class="col-sm-10" style="height: 3rem">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Selecciona el área de servicio</option>
+                        <option value="1">Bomberos</option>
+                        <option value="2">Paramédicos</option>
+                        <option value="3">Inspección</option>
+                        <option value="4">Administrativo</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Unidad</label>
+                <div class="col-sm-10" style="height: 3rem">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Selecciona la Unidad</option>
+                        <option value="1">PCM 01</option>
+                        <option value="2">PCM 04</option>
+                        <option value="3">PCM 06</option>
+                        <option value="4">PCM 17</option>
+                        <option value="5">PCM 21</option>
+                        <option value="6">PCM 22</option>
+                        <option value="7">PCM 23</option>
+                        <option value="8">PCM 1077</option>
+                        <option value="9">SUMICH 1370</option>
+                        <option value="10">SUMICH 1614</option>
+                        <option value="12">BMT 01</option>
+                        <option value="13">BMT 02</option>
+                        <option value="14">BMT 03</option>
+                        <option value="15">BMT 04</option>
+                        <option value="16">BMT 05</option>
+                        <option value="17">BMT 06</option>
+                        <option value="18">BMT 07</option>
+                        <option value="19">BMT 08</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Incidente</label>
+                <div class="col-sm-10" style="height: 3rem">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Selecciona el tipo de incidente</option>
+                        <option value="1">Aborto o Amenaza de Aborto</option>
+                        <option value="2">Accidente Vehicular</option>
+                        <option value="3">Agresión Física sin Armas</option>
+                        <option value="4">Aplastamiento</option>
+                        <option value="5">Apoyo a bomberos </option>
+                        <option value="6">Apoyo a Municipios</option>
+                        <option value="7">Atropellado</option>
+                        <option value="8">Caída</option>
+                        <option value="9">Choque Vehicular</option>
+                        <option value="10">Cobertura de Evento</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Ubicacion</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPassword" placeholder="Agregar ubicación">
+                </div>
+            </div>
+        </form>
+        </div>
+    </div>
 </div>
