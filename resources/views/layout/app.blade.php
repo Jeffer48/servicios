2c427659caaf1d1daf8b954e4eab261e4f28f141
Jeffer48 <herrerajeffer@gmail.com>
2024-03-12 16:13:41 -0600

    problemas con sidebar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,12 +10,12 @@
         <link href="css/content.css" rel="stylesheet">
     </head>
 
-    <body style="display: flex">
-        <section>
-            <div class="d-flex flex-nowrap" style="height: 100%;">
+    <body style="display: grid;">
+        <div style="grid-column: 1;">
+            <div class="d-flex flex-nowrap" style="height: 100vh; position: fixed;">
                 <h1 class="visually-hidden">Sidebars</h1>
         
-                <div class="d-flex flex-column flex-shrink-0 p-3" style="width: 280px; background-color: #152733;">
+                <div class="d-flex flex-column flex-shrink-0 p-3" style="background-color: #152733;">
                     <a href="/" style="margin-left: 5rem; margin-top: 1rem;" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                         <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" fill="currentColor" class="bi bi-stack" viewBox="0 0 16 16">
                             <path d="m14.12 10.163 1.715.858c.22.11.22.424 0 .534L8.267 15.34a.6.6 0 0 1-.534 0L.165 11.555a.299.299 0 0 1 0-.534l1.716-.858 5.317 2.659c.505.252 1.1.252 1.604 0l5.317-2.66zM7.733.063a.6.6 0 0 1 .534 0l7.568 3.784a.3.3 0 0 1 0 .535L8.267 8.165a.6.6 0 0 1-.534 0L.165 4.382a.299.299 0 0 1 0-.535z"/>
@@ -81,10 +81,10 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
 
-        <section style="width: -webkit-fill-available;">
-            <div id="header">
+        <div class="layout-content" style="grid-column: 2;">
+            <div id="header" style="position: fixed;">
                 <nav class="navbar" style="background-color: #6A0F49; height: 3.5rem">
                     <div class="nav_container">
                         <div style="align-self: center;">
@@ -102,7 +102,7 @@
                     </div>
                 </nav>
             </div>
-            <div style="display: flex;">
+            <div style="display: flex; padding-top: 4%;">
                 @yield('content')
                 <div class="active_services">
                     <div class="target_services">
@@ -110,7 +110,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </div>
 
         <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
         <script src="sidebars.js"></script>
--- resources/views/reporte_radio.blade.php
+++ resources/views/reporte_radio.blade.php
@@ -3,8 +3,37 @@
 @section('content')
 <div class="container" id="form_container">
     <h1 class="title_form">Reporte de Radio</h1>
-    <div class="separator"></div>
     <form class="principal_form">
+        <div class="row mb-3">
+            <label class="col-sm-3 input-label">Fecha</label>
+            <div class="col-sm-9">
+            <input type="datetime-local" class="form-control" id="input_date">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-3 input-label">Área</label>
+            <div class="col-sm-9">
+            <input type="text" class="form-control" id="input_area">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-3 input-label">Unidad</label>
+            <div class="col-sm-9">
+            <input type="text" class="form-control" id="input_unit">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-3 input-label">Incidente</label>
+            <div class="col-sm-9">
+            <input type="text" class="form-control" id="input_incident">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-3 input-label">Ubicación</label>
+            <div class="col-sm-9">
+            <input type="text" class="form-control" id="input_location">
+            </div>
+        </div>
         <div class="row mb-3">
             <label class="col-sm-3 input-label">Fecha</label>
             <div class="col-sm-9">
