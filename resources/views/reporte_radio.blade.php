f48ada3b39ff107101de032749e3d56edd7753b1
Jeffer48 <herrerajeffer@gmail.com>
2024-03-13 10:49:35 -0600

    Removido doble contenido de radio

--- resources/views/reporte_radio.blade.php
+++ resources/views/reporte_radio.blade.php
@@ -34,36 +34,6 @@
             <input type="text" class="form-control" id="input_location">
             </div>
         </div>
-        <div class="row mb-3">
-            <label class="col-sm-3 input-label">Fecha</label>
-            <div class="col-sm-9">
-            <input type="datetime-local" class="form-control" id="input_date">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-3 input-label">Área</label>
-            <div class="col-sm-9">
-            <input type="text" class="form-control" id="input_area">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-3 input-label">Unidad</label>
-            <div class="col-sm-9">
-            <input type="text" class="form-control" id="input_unit">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-3 input-label">Incidente</label>
-            <div class="col-sm-9">
-            <input type="text" class="form-control" id="input_incident">
-            </div>
-        </div>
-        <div class="row mb-3">
-            <label class="col-sm-3 input-label">Ubicación</label>
-            <div class="col-sm-9">
-            <input type="text" class="form-control" id="input_location">
-            </div>
-        </div>
         <div class="text-center" style="margin: 2rem;">
             <button type="submit" class="btn btn-success">Registrar</button>
         </div>
