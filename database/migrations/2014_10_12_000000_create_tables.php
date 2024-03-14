ddfc2cc061177ad723df910be4d9984053a46329
Jeffer48 <herrerajeffer@gmail.com>
2024-03-14 10:03:06 -0600

    Tablas catalogos y radio

--- database/migrations/2014_10_12_000000_create_tables.php
+++ database/migrations/2014_10_12_000000_create_tables.php
@@ -8,75 +8,103 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('grupos');
-        Schema::dropIfExists('catalogo');
-        Schema::dropIfExists('reporte_radio');
-
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('adm_grupos', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('user');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('grupo',100)->nullable(false);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->softDeletes();
-            $table->string('created_user',45)->nullable(false);
-            $table->string('updated_user',45)->nullable(true);
-            $table->string('deleted_user',45)->nullable(true);
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
         });
 
-        Schema::create('grupos', function(Blueprint $table) {
-            $table->increments('id');
-            $table->string('grupo')->nullable(false);
-            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->softDeletes();
-            $table->string('created_user',45)->nullable(false);
-            $table->string('updated_user',45)->nullable(true);
-            $table->string('deleted_user',45)->nullable(true);
-        });
-
-        Schema::create('catalogo', function(Blueprint $table) {
+        Schema::create('adm_catalogos', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('id_grupo')->unsigned()->nullable(false);
+            $table->string('clave',20)->nullable(true);
             $table->string('descripcion',255)->nullable(false);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->softDeletes();
-            $table->string('created_user',45)->nullable(false);
-            $table->string('updated_user',45)->nullable(true);
-            $table->string('deleted_user',45)->nullable(true);
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
+            
+            $table->foreign('id_grupo')->references('id')->on('adm_grupos');
+        });
+
+        Schema::create('adm_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_tipo')->unsigned()->nullable(false);
+            $table->string('nombre',50)->nullable(false);
+            $table->string('apellido_p',50)->nullable(true);
+            $table->string('apellido_m',50)->nullable(true);
+            $table->string('correo')->unique()->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
+
+            $table->foreign('id_tipo')->references('id')->on('adm_catalogos');
+        });
+
+        Schema::create('grupos', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('grupo',100)->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
+        });
+
+        Schema::create('catalogos', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_grupo')->unsigned()->nullable(false);
+            $table->string('clave',20)->nullable(true);
+            $table->string('descripcion',255)->nullable(false);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
             
             $table->foreign('id_grupo')->references('id')->on('grupos');
         });
 
         Schema::create('reporte_radio', function(Blueprint $table) {
             $table->increments('id');
-            $table->date('fecha')->nullable(false);
-            $table->time('hora')->nullable(false);
+            $table->timestamp('fecha')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->integer('id_area')->unsigned()->nullable(false);
             $table->integer('id_unidad')->unsigned()->nullable(false);
-            $table->integer('id_tipo_incidente')->unsigned()->nullable(false);
-            $table->string('ubicacion',255)->nullable(false);
+            $table->integer('id_incidente')->unsigned()->nullable(false);
+            $table->string('ubicación',255)->nullable(true);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->softDeletes();
-            $table->string('created_user',45)->nullable(false);
-            $table->string('updated_user',45)->nullable(true);
-            $table->string('deleted_user',45)->nullable(true);
-
-            $table->foreign('id_area')->references('id')->on('catalogo');
-            $table->foreign('id_unidad')->references('id')->on('catalogo');
-            $table->foreign('id_tipo_incidente')->references('id')->on('catalogo');
+            $table->integer('created_user')->unsigned()->nullable(true);
+            $table->integer('updated_user')->unsigned()->nullable(true);
+            $table->integer('deleted_user')->unsigned()->nullable(true);
+            
+            $table->foreign('id_area')->references('id')->on('catalogos');
+            $table->foreign('id_unidad')->references('id')->on('catalogos');
+            $table->foreign('id_incidente')->references('id')->on('catalogos');
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('grupos');
-        Schema::dropIfExists('catalogo');
+        Schema::dropIfExists('adm_user');
+        Schema::dropIfExists('adm_catalogos');
+        Schema::dropIfExists('adm_grupos');
         Schema::dropIfExists('reporte_radio');
+        Schema::dropIfExists('grupos');
+        Schema::dropIfExists('catalogos');
     }
 };
